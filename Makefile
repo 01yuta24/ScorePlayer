f7c9a8403ffbf82271fcf8fbd6518031285f5ab8
Otenpu- <yuta.mybusiness@gmail.com>
2026-01-14 08:10:45 +0900

    make test lint の実装

--- Makefile
+++ Makefile
@@ -1,2 +1,15 @@
 test_FE:
 	cd ./frontend && make test
+
+lint_FE:
+	cd ./frontend && make lint
+
+test_e2e:
+	cd ./e2e && make test
+
+lint_e2e:
+	cd ./e2e && make lint
+
+test: test_FE test_e2e
+
+lint : lint_FE lint_e2e
--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,8 +1,8 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
 test("ボタンを押すと /home に遷移する", async ({ page }) => {
   await page.goto("http://localhost:5173/");
-    const button = page.getByRole("button", { name: "ボタン" });
-    await button.click();
-    await expect(page).toHaveURL("http://localhost:5173/home");
-})
+  const button = page.getByRole("button", { name: "スタート" });
+  await button.click();
+  await expect(page).toHaveURL("http://localhost:5173/home");
+});
