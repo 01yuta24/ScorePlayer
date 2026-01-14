c6d2c75cfcdba17d230bfd4e00bc54e3d5bec863
Otenpu- <yuta.mybusiness@gmail.com>
2026-01-15 08:56:18 +0900

    backend,spring securityの導入

---
+++ backend/src/test/kotlin/sasa_cchi/score_player/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package sasa_cchi.score_player.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
